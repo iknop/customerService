08c24abf9890f83b58d04a1507b12b9b6cf00d97
snzjiin <kji306301@gmail.com>
2022-11-10 11:13:15 +0900

    - 화면 작업중: 처리내용 글자수 제한

--- src/main/resources/static/js/show/getResponseResultList.js
+++ src/main/resources/static/js/show/getResponseResultList.js
@@ -1,6 +1,7 @@
-$(
-    // 처리내용 리스트 불러오기
-    getResultList()
+$(function () {
+        // 처리내용 리스트 불러오기
+        getResultList()
+    }
 )
 
 // 처리내용 리스트 불러오기
@@ -19,35 +20,35 @@ function getResultList() {
         // todo tag.removechild All
         tag.innerHTML = '';
         // 처리내용이 존재할 때 테이블 셀 활성화
-        if(loop>0){
+        if (loop > 0) {
             tag.classList.remove('d-none')
         }
 
         for (let i = 0; i < loop; i++) {
             let div = document.createElement('div');
-            div.classList.add('row','justify-content-between','pl-4')
+            div.classList.add('row', 'justify-content-between', 'pl-4')
 
             // 처리내용 본문
             let content = document.createElement('span');
-            content.classList.add('col-8','mr-1','px-0')
+            content.classList.add('col-8', 'mr-1', 'px-0')
             content.id = `resultContents-${response[i].rrId}`;
             content.innerText = response[i].resultContents;
 
             //처리내용 수정 textarea: class d-none -> 안보이게 (*수정 시 데이터*)
             let textAreaElement = document.createElement('textarea');
-            textAreaElement.classList.add('form-control','d-none','mr-1','ResponseResultTextArea');
+            textAreaElement.classList.add('form-control', 'd-none', 'mr-1', 'ResponseResultTextArea');
             textAreaElement.id = `textarea-${response[i].rrId}`;
             textAreaElement.innerText = response[i].resultContents;
 
             //처리내용 작성 시간
             let createTime = document.createElement('div');
-            createTime.classList.add('createDateTime','col-2','px-0', 'mx-3');
+            createTime.classList.add('createDateTime', 'col-2', 'px-0', 'mx-3');
             let dateStr = getDateStr(new Date(response[i].createDateTime))
             createTime.innerText = dateStr;
 
             // 처리내용 작성자 이름
             let name = document.createElement('div');
-            name.classList.add('createUserName','col-1','px-0', 'mr-2');
+            name.classList.add('createUserName', 'col-1', 'px-0', 'mr-2');
             name.id = `name-${response[i].rrId}`;
             name.innerText = response[i].createUserName;
             name.setAttribute('name', 'createUserName');
@@ -55,7 +56,7 @@ function getResultList() {
 
             // 처리내용 작성자 아이디(*수정 삭제 버튼 활성*)
             let resultCreateUser = document.createElement('input');
-            resultCreateUser.setAttribute('type','hidden')
+            resultCreateUser.setAttribute('type', 'hidden')
             resultCreateUser.value = response[i].createUserCode;
 
 
@@ -75,7 +76,7 @@ function getResultList() {
 
             // 수정버튼 생성 및 이벤트 추가
             let buttons = document.createElement('div')
-            buttons.classList.add('row','justify-content-end','pr-5','my-2')
+            buttons.classList.add('row', 'justify-content-end', 'pr-5', 'my-2')
 
             let updateBtn = document.createElement('button');
             $(updateBtn).val(response[i].rrId);
@@ -110,7 +111,7 @@ function getResultList() {
             // 로그인 아이디 == 서비스 내용 등록자 -> 수정버튼 표시
             let loginId = $('#loginId').val();
             console.log(resultCreateUser.value)
-            if(loginId == resultCreateUser.value){
+            if (loginId == resultCreateUser.value) {
                 deleteBtn.classList.remove('d-none')
                 updateBtn.classList.remove('d-none')
                 // updateBtn.removeClass('d-none')
--- src/main/resources/static/js/show/insertResponseResult.js
+++ src/main/resources/static/js/show/insertResponseResult.js
@@ -1,6 +1,19 @@
 $(function () {
     let loginId = $('#loginId').val(); // 헤더
     $('#resultLoginId').val(loginId)// 처리내용입력 아이디
+
+    $('#responseResultInsert').keyup(function (e) {
+        let content = $(this).val();
+        console.log(content)
+        // 글자수 제한
+        if (content.length > 1000) {
+            // 타이필 불가
+            $(this).val($(this).val().substring(0, 1000));
+            // 입력불가 알림
+            alert('1000자까지만 입력 가능합니다.');
+        }
+
+    })
 })
 // 처리내용 등록 버튼 onclick
 function insertResult() {
